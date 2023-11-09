31e453782a0ba9ecf7b607c62b860f8fdff2927f
AMAYADIEGO <amayadiego839@gmail.com>
2023-11-08 21:26:52 -0400

    tarea1

---
+++ TAREA1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace tarea1
+{
+    class program 
+    {
+    static void  Main(string[] args)
+    {
+        Console.WriteLine("hello world");
+        int resp = CalcularAreaCuadrado(10);
+        Console.WriteLine(resp);
+    }
+
+    static int CalcularAreaCuadrado(int L){
+        int resultado = L*L;
+        return resultado;
+    }
+    }
+}
