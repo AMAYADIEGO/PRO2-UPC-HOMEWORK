901b620bb8ce6aa501cfe488c88010d4ea4289d0
AMAYADIEGO <132412290+AMAYADIEGO@users.noreply.github.com>
2024-08-08 22:00:25 -0400

    Add files via upload

---
+++ Nueva carpeta (3)/ConsoleApp1/ConsoleApp1/MemoriaImp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memoria
+{
+    public class MemoriaImp : MemoriaAbs
+    {
+        public override void mostrar()
+        {
+            int dir = 0;
+            Console.WriteLine("dir\t| dato\t| link");
+            Console.WriteLine("---------------------");
+
+            foreach (NodoM elemento in MEM)
+            {
+                int dato = elemento.dato;
+                int link = elemento.link;
+
+                Console.WriteLine("" + dir + "\t|" + dato + "\t|" + link);
+                dir++;
+            }
+        }
+        public override void new_espacio(int cantidad)
+        {
+            int dir = libre;
+            int D = libre;
+            int i = 1;
+            while (i < cantidad -1 )
+            {
+                D = MEM[D].link;
+                libre = MEM[D].link;
+                MEM[D].link = -1;
+                
+            }
+            
+        }
+    }
+}
---
+++ Nueva carpeta (3)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+ namespace Memoria
+{
+    class ProgramaPrincipal
+    {
+        static void Main(string[] args)
+        {
+            MemoriaImp memoria = new MemoriaImp();
+
+            memoria.mostrar();
+        }
+    }
+    
+    
+}
